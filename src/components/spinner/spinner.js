5a163c31f9935e99fc48f1c7ccaa30a706d94d0c
Andrew Salmanov <ads739912@gmail.com>
2023-03-28 12:49:36 +0300

    Spinner complete

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,247 @@
+const Spinner = () => {
+        return (
+                <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        style={{
+                                margin: "auto",
+                                background: "none",
+                                WebkitAnimationPlayState: "running",
+                                animationPlayState: "running",
+                                WebkitAnimationDelay: "0s",
+                                animationDelay: "0s",
+                        }}
+                        width="164"
+                        height="164"
+                        display="block"
+                        preserveAspectRatio="xMidYMid"
+                        viewBox="0 0 100 100"
+                >
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#f84828"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-1.559454191033138s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#68c0c0"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(40 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-1.3645224171539958s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#f8f0e0"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(80 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-1.1695906432748535s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#cba331"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(120 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-0.9746588693957113s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#7bbab0"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(160 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-0.779727095516569s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#f84828"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(200 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-0.5847953216374268s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#68c0c0"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(240 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-0.3898635477582845s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#f8f0e0"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(280 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="-0.19493177387914226s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                        <rect
+                                width="6"
+                                height="15"
+                                x="47"
+                                y="25.5"
+                                fill="#cba331"
+                                rx="3"
+                                ry="5.4"
+                                style={{
+                                        WebkitAnimationPlayState: "running",
+                                        animationPlayState: "running",
+                                        WebkitAnimationDelay: "0s",
+                                        animationDelay: "0s",
+                                }}
+                                transform="rotate(320 50 50)"
+                        >
+                                <animate
+                                        attributeName="opacity"
+                                        begin="0s"
+                                        dur="1.7543859649122806s"
+                                        keyTimes="0;1"
+                                        repeatCount="indefinite"
+                                        values="1;0"
+                                ></animate>
+                        </rect>
+                </svg>
+        );
+}
+
+export default Spinner;
