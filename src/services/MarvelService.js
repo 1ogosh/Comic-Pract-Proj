07b545788acda3b64978cc45359254f53c1b2070
Andrew Salmanov <ads739912@gmail.com>
2023-04-02 17:52:40 +0300

    updateable part

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,38 +1,39 @@
-
-
 class MarvelService {
         _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-        _apiKey = 'apikey=f9a2b54eb25030335258daa3f91ff474';
-
+        // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+        _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
+    
         getResource = async (url) => {
-                let res = await fetch(url);
-
-                if (!res.ok) {
-                        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-                }
-
-                return await res.json();
+            let res = await fetch(url);
+        
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+            }
+        
+            return await res.json();
         }
-
+    
         getAllCharacters = async () => {
-                const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-                return res.data.results.map(this._transformCharacter);
+            const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+            return res.data.results.map(this._transformCharacter);
         }
-
+    
         getCharacter = async (id) => {
-                const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-                return this._transformCharacter(res.data.results[0]);
+            const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+            return this._transformCharacter(res.data.results[0]);
         }
-
+    
         _transformCharacter = (char) => {
-                return {
-                        name: char.name,
-                        description: char.description,
-                        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-                        homepage: char.urls[0].url,
-                        wiki: char.urls[1].url
-                }
+            return {
+                id: char.id,
+                name: char.name,
+                description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+                thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+                homepage: char.urls[0].url,
+                wiki: char.urls[1].url
+            }
         }
-}
-
-export default MarvelService;
+    }
+    
+    export default MarvelService;
+   
